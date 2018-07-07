910de42278215541b808347d8cb14b9d52fee5f2
Jacob Bendicksen <jacob@jacobbendicksen.com>
2018-07-07 12:42:26 -0700

    initial commit, get/post routes

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let express = require('express');
+let exphbs = require('express-handlebars');
+let bodyparser = require('body-parser');
+
+let app = express();
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+app.use(bodyparser.urlencoded({
+    extended: true,
+}));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/name', (req, res) => {
+    res.render('index', {name: req.body.name});
+});
+
+app.listen(4000);
