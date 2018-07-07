6660cd09143dc94d582a4058dd866cc4de2c2366
Jacob Bendicksen <jacob@jacobbendicksen.com>
2018-07-07 13:28:32 -0700

    start of testing framework

---
+++ test/test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('GET', function() {
+  describe('Home page', function() {
+    it('should greet with "Hello world" on GET', function() {
+      //in progress
+    });
+  });
+});
+
+describe('POST', function() {
+  describe('Home page', function() {
+    it('should greet with provided name', function() {
+      //in progress
+    });
+    it('should default to "Hello world" with no provided name', function() {
+      //in progress
+    });
+  });
+});
